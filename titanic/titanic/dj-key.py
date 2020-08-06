036e83efb46c345f0cb2ba7b32c0dc1ed0714cf8
dorinzrihen <zrihend@gmail.com>
2020-08-06 23:57:28 +0300

    titanic css

---
+++ titanic/titanic/dj-key.py
@@ -0,0 +1,3 @@
+def get_key():
+    return 'n^u7frxkebwf=sb24&^q-gq+r4#e-$ncir7(@z92h@ra(y8gmz'
+
--- titanic/titanic/settings.py
+++ titanic/titanic/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -21,6 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'n^u7frxkebwf=sb24&^q-gq+r4#e-$ncir7(@z92h@ra(y8gmz'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -116,5 +118,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+import os.path
 STATIC_URL = '/static/'
+STATICFILES_DIRS=(os.path.join('static'),)
